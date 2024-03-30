6229852d170deb4c4a5a9347c02d4d609d673533
AlexTruee <133477855+AlexTruee@users.noreply.github.com>
2024-03-30 12:01:35 +0500

    First commit

---
+++ measurement/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from measurement.views import SensorCreateView, SensorUpdateView, MeasurementCreateView
+
+urlpatterns = [
+    path('sensors/', SensorCreateView.as_view()),
+    path('sensors/<pk>/', SensorUpdateView.as_view()),
+    path('measurements/', MeasurementCreateView.as_view()),
+]
